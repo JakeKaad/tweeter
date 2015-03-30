b4fcb5ad09724a75fde85ea02120ad7da7b81406
Jessica Hori and Jake Kaad <kaadalac@gmail.com>
2015-03-30 11:19:14 -0700

    Add sign in and sign out links with tests

--- spec/features/user_sessions_spec.rb
+++ spec/features/user_sessions_spec.rb
@@ -8,16 +8,33 @@ feature 'registers a new user' do
   end
 
   scenario 'it creates a new user' do
-    visit '/'
-    click_link 'Register'
-    fill_in 'Email', with: "test@test.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_on "Sign up"
+    create_new_user
     expect(page).to have_content("Welcome!")
   end
 end
 
+feature 'logs a user out and in' do
+  scenario 'it logs a user out and in' do
+    create_new_user
+    click_link 'Sign out'
+    expect(page).to have_content("Signed out successfully.")
 
+    click_link 'Sign in'
+    expect(page).to have_content("Log in")
+    fill_in "Email", with: "test@test.com"
+    fill_in "Password", with: "password"
+    click_on "Log in"
+    expect(page).to have_content("Signed in successfully.")
+  end
+end
+
+def create_new_user
+  visit '/'
+  click_link 'Register'
+  fill_in 'Email', with: "test@test.com"
+  fill_in "Password", with: "password"
+  fill_in "Password confirmation", with: "password"
+  click_on "Sign up"
+end
 #save_and_open_page opens the page using the 'launchy' gem
 #Click on: buttons
